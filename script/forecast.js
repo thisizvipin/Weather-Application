a94cb08504407da2eaed97ae723928c3f04c687b
thisizvipin <thisizvipin@gmail.com>
2020-10-24 14:28:21 +0530

    weather app

---
+++ script/forecast.js
@@ -0,0 +1,23 @@
+const key = "XuAs5UjPbUrND7F21Wlgz1F0RzjJMUvC";
+
+// get weather info
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city info
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
